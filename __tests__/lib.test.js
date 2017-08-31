f5f9f120be3448573831284cbb46dcfc48769fc7
Luiz Kota <luiz.kota@catho.com>
2017-08-31 09:39:45 -0300

    Create a custon exception; Unit test matching the exact exception

--- __tests__/lib.test.js
+++ __tests__/lib.test.js
@@ -1,12 +1,17 @@
 const lib = require('../lib/index.js');
+const ParameterException = require('../lib/exceptions/parameterException.js');
 
 describe('# Testing render configuration', () => {
   it('Calling the render without the template parameter, I expect that returns a exception', () => {
     expect(() => lib())
-      .toThrow();
+      .toThrow(
+        new ParameterException('template is mandatory')
+      );
   });
   it('Calling the render with the template parameter different of a string, I expect that returns a exception', () => {
     expect(() => lib(42))
-      .toThrow();
+      .toThrow(
+        new ParameterException('template must be a string')
+      );
   });
 });
