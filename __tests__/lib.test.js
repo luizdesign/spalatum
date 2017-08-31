b5e95224f4317b416455bb70408cee475741e9d2
Luiz Kota <luiz.kota@catho.com>
2017-08-31 20:09:55 -0300

    Testing the render with fragments

--- __tests__/lib.test.js
+++ __tests__/lib.test.js
@@ -1,5 +1,20 @@
 const lib = require('../lib/index.js');
+const http = require('http');
+const fs = require('fs');
 const ParameterException = require('../lib/exceptions/parameterException.js');
+
+const mockServer = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(require('../__mocks__/fragment.js'));
+  res.end();
+});
+beforeEach(() => {
+  mockServer.listen(8000);
+});
+afterEach(() => {
+  mockServer.close();
+});
+
 
 describe('# Testing render configuration', () => {
   it('Calling the render without the template parameter, I expect that returns a exception', () => {
@@ -16,11 +31,34 @@ describe('# Testing render configuration', () => {
   });
 });
 
-describe('# Testing a template without fragments', () => {
-  it('Calling the render with the template with no fragments, I expect that returns the same template', async () => {
+describe('# Testing a template without fragments', async () => {
+  it('Calling the lib with the template with no fragments, I expect that returns the same template', async () => {
     const mock = '<html><head></head><body><h1>Unit test</h1></body></html>';
     const template = await lib(mock);
 
     expect(template).toEqual(mock);
+  });
+});
+
+describe('# Testing a template with fragments', async () => {
+  it('Calling the render with the template with fragments, I expect that returns the template with the fragments rendered', async () => {
+    const mock = require('../__mocks__/simple-fragment.js');
+    const response = require('../__mocks__/fragment.js');
+
+    const template = await lib(mock);
+    expect(template).toEqual(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Unit Test</title>
+  </head>
+  <body>
+    <section>
+  <header>
+    <h1>This is a Fragment</h1>
+  </header>
+</section>
+  </body>
+</html>`)
   });
 });
