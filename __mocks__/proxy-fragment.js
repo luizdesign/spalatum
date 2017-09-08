5ad9ae68014d779f54e6e4aa377491316dec0da3
Luiz Kota <luiz.kota@catho.com>
2017-09-08 08:47:38 -0300

    Adding test scenario for fragment with proxy

---
+++ __mocks__/proxy-fragment.js
@@ -0,0 +1,10 @@
+module.exports = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Unit Test</title>
+  </head>
+  <body>
+    <fragment href="http://localhost:7000/" proxy="localhost:5000"></fragment>
+  </body>
+</html>`;
