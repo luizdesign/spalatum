1084ef7464ba6d1bfc698811c0abc679c873c902
Luiz Kota <luiz.kota@catho.com>
2017-09-08 10:22:22 -0300

    Adding unit test to attributes module

---
+++ __tests__/fragment-attributes.test.js
@@ -0,0 +1,27 @@
+const attributesModule = require('../lib/fragment-attributes');
+
+describe('# Testing the fragment-attributes module', () => {
+  it('Calling the module with <fragment href="localhost:3000" proxy="localhost:2000" />, I expect that returns an object with the atrributes href and proxy', () => {
+    expect(attributesModule('<fragment href="localhost:3000" proxy="localhost:2000" />'))
+      .toEqual({
+        href: 'localhost:3000',
+        proxy: 'localhost:2000',
+      });
+  });
+
+  it('Calling the module with <fragment href="localhost:3000" />, I expect that returns an object with the atrributes href and proxy but proxy equal to null', () => {
+    expect(attributesModule('<fragment href="localhost:3000" />'))
+      .toEqual({
+        href: 'localhost:3000',
+        proxy: null,
+      });
+  });
+
+  it('Calling the module with <fragment href="localhost:3000" proxy="" />, I expect that returns an object with the atrributes href and proxy but proxy empty', () => {
+    expect(attributesModule('<fragment href="localhost:3000" proxy="" />'))
+      .toEqual({
+        href: 'localhost:3000',
+        proxy: null,
+      });
+  });
+});
