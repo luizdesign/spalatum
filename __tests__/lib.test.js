9a86ee80171aa77ff96174caaef2fd7436412c8c
Daniel Silva <daniel.silva@catho.com>
2017-09-11 15:26:39 -0300

    Using superagent for requests

--- __tests__/lib.test.js
+++ __tests__/lib.test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const ParameterException = require('../lib/exceptions/parameterException.js');
 
 beforeEach(() => {
-  document.body.innerHTML = null;
+  document.body.outerHTML = null;
 });
 
 describe('# Testing render configuration', () => {
@@ -26,35 +26,35 @@ describe('# Testing a template without fragments', async () => {
     const originalTemplate = '<html><head></head><body><h1>Unit test</h1></body></html>';
     const renderedTemplate = await lib(originalTemplate);
 
-    document.body.innerHTML = renderedTemplate;
-    expect(document.body.innerHTML).toMatchSnapshot();
+    document.body.outerHTML = renderedTemplate;
+    expect(document.body.outerHTML).toMatchSnapshot();
   });
 });
 
 describe('# Testing a template with fragments', async () => {
   it('Calling the render with the template with fragments, I expect that returns the template with the fragments rendered', async () => {
-    const originalTemplate = require('../__mocks__/simple-fragment.js');
+    const originalTemplate = require('../__mocks__/simple-template.js');
     const mockServer = require('../__mocks__/server.js')('../__mocks__/fragment.js');
     mockServer.listen(8000);
 
     const renderedTemplate = await lib(originalTemplate);
-    document.body.innerHTML = renderedTemplate;
-    expect(document.body.innerHTML).toMatchSnapshot();
+    document.body.outerHTML = renderedTemplate;
+    expect(document.body.outerHTML).toMatchSnapshot();
     mockServer.close();
   });
 });
 
 describe('# Testing a template with fragments using proxy', async () => {
   it('Calling the render with the template with fragments using proxy, I expect that returns the template with the fragments rendered', async () => {
-    const originalTemplate = require('../__mocks__/proxy-fragment.js');
+    const originalTemplate = require('../__mocks__/proxy-template.js');
     const mockServer = require('../__mocks__/server.js')('../__mocks__/fragment.js');
     const mockProxyServer = require('../__mocks__/proxy-server.js')('http://localhost:7000');
     mockProxyServer.listen(5000);
     mockServer.listen(7000);
 
     const renderedTemplate = await lib(originalTemplate);
-    document.body.innerHTML = renderedTemplate;
-    expect(document.body.innerHTML).toMatchSnapshot();
+    document.body.outerHTML = renderedTemplate;
+    expect(document.body.outerHTML).toMatchSnapshot();
     mockServer.close();
     mockProxyServer.close();
   });
