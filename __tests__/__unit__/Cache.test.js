00559d6556ae33b90c0bfb9563ec04e808fb2a42
Daniel Silva <daniel.silva@catho.com>
2017-10-18 14:26:01 -0200

    Improve coverage

--- __tests__/__unit__/Cache.test.js
+++ __tests__/__unit__/Cache.test.js
@@ -4,9 +4,10 @@ const responseMock = require('../../__mocks__/response');
 const mockResponseSmaller50 = 'unit test unit test unit test unit test unit test';
 const mockResponseGreather50 = `${mockResponseSmaller50} test unit test`;
 const mockContentType = 'text/html';
+const instance = new Cache();
 
 describe('# Testing the fragment-caching module', () => {
-  describe('# CanSaveCache method', () => {
+  describe('# CanSaveCache static method', () => {
     const execTest = (mock, result) => expect(Cache.canSave('10s', mock)).toEqual(result);
 
     it('Calling the module with max age null, I expect that returns false', () => {
@@ -31,6 +32,42 @@ describe('# Testing the fragment-caching module', () => {
 
     it('Calling the module with max age different of null, httpRequest equal to 200, content size greather than 200 and content-type equal to text/html, I expect that returns true', () => {
       execTest(responseMock(200, mockResponseGreather50, 'text/html'), true);
+    });
+  });
+
+  describe('# Save method', () => {
+    const testValues = [{
+      key: 'foo',
+      content: 'foo content',
+      maxAge: '10m',
+    }, {
+      key: 'bar',
+      content: 'bar content',
+      maxAge: '10m',
+    }];
+
+    beforeEach(() => {
+      instance.clearAll();
+    });
+
+    it('Calling the method with an uncached value. I expect the value is cached', () => {
+      const testItem = testValues[0];
+
+      expect(instance.isCached(testItem.key)).toBeFalsy();
+      expect(instance.getItem(testItem.key)).toBeNull();
+
+      instance.save(testItem.key, testItem.maxAge, testItem.content);
+
+      expect(instance.isCached(testItem.key)).toBeTruthy();
+      expect(instance.getItem(testItem.key).content).toEqual(testItem.content);
+    });
+
+    it('Calling the method with an cached value. I expect the cache object have only one item', () => {
+      const testItem = testValues[0];
+      instance.save(testItem.key, testItem.maxAge, testItem.content);
+      instance.save(testItem.key, testItem.maxAge, testItem.content);
+
+      expect(Object.keys(instance.cacheObject).length).toBe(1);
     });
   });
 });
