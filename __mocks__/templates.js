608bcdd3aebd0a4a6b6c3d1d33129827eebf2813
Daniel Silva <daniel.silva@catho.com>
2017-10-18 11:59:47 -0200

    Templates

--- __mocks__/templates.js
+++ __mocks__/templates.js
@@ -28,32 +28,33 @@ const templateBuilder = (...fragments) => {
 const primary = true;
 const cache = '10m';
 const proxy = 'http://localhost:5000/';
+const href = 'http://localhost:8000/';
 
 module.exports = {
   clean: templateBuilder(),
-  simple: templateBuilder('http://localhost:8000/'),
+  simple: templateBuilder(href),
   https: templateBuilder('https://httpbin.org/html'),
   error: templateBuilder('http://localhost:1000/'),
   notFound: templateBuilder('https://httpbin.org/notfound/'),
   primary: templateBuilder(
-    { href: 'http://localhost:8000/', primary },
+    { href, primary },
+  ),
+  proxy: templateBuilder(
+    { href: 'http://localhost:7000/', proxy },
   ),
   singleCache: templateBuilder(
     { href: 'http://localhost:9000/', cache },
   ),
-  proxy: templateBuilder(
-    { href: 'http://localhost:7000/', proxy },
+  cache: templateBuilder(
+    { href: 'http://localhost:9000/', cache },
+    { href: 'http://localhost:9001/', cache },
   ),
   notFoundPrimary: templateBuilder(
     { href: 'https://httpbin.org/notfound/', primary },
     { href: 'https://httpbin.org/' },
   ),
   twoPrimary: templateBuilder(
-    { href: 'http://localhost:8000/', primary },
-    { href: 'http://localhost:8000/', primary },
-  ),
-  cache: templateBuilder(
-    { href: 'http://localhost:9000/', cache },
-    { href: 'http://localhost:9001/', cache },
+    { href, primary },
+    { href, primary },
   ),
 };
