125301d624947dd32cf63285ae911a32ea430042
Daniel Silva <daniel.silva@catho.com>
2017-10-18 10:53:12 -0200

    Refactoring cache tests

--- __tests__/__unit__/Cache.test.js
+++ __tests__/__unit__/Cache.test.js
@@ -4,43 +4,33 @@ const responseMock = require('../../__mocks__/response');
 const mockResponseSmaller50 = 'unit test unit test unit test unit test unit test';
 const mockResponseGreather50 = `${mockResponseSmaller50} test unit test`;
 const mockContentType = 'text/html';
-let mock;
 
 describe('# Testing the fragment-caching module', () => {
   describe('# CanSaveCache method', () => {
+    const execTest = (mock, result) => expect(Cache.canSave('10s', mock)).toEqual(result);
+
     it('Calling the module with max age null, I expect that returns false', () => {
       expect(Cache.canSave(null, {})).toEqual(false);
     });
+
     it('Calling the module with max age different of null and httpRequest different of 200, I expect that returns false', () => {
-      mock = responseMock(301, mockResponseGreather50, mockContentType);
-      expect(Cache.canSave('10s', mock)).toEqual(false);
-
-      mock = responseMock(404, mockResponseGreather50, mockContentType);
-      expect(Cache.canSave('10s', mock)).toEqual(false);
-
-      mock = responseMock(500, mockResponseGreather50, mockContentType);
-      expect(Cache.canSave('10s', mock)).toEqual(false);
+      execTest(responseMock(301, mockResponseGreather50, mockContentType), false);
+      execTest(responseMock(301, mockResponseGreather50, mockContentType), false);
+      execTest(responseMock(500, mockResponseGreather50, mockContentType), false);
     });
 
     it('Calling the module with max age different of null, httpRequest equal to 200 and content size smaller than 200, I expect that returns false', () => {
-      mock = responseMock(200, '', mockContentType);
-      expect(Cache.canSave('10s', mock)).toEqual(false);
-
-      mock = responseMock(200, mockResponseSmaller50, mockContentType);
-      expect(Cache.canSave('10s', mock)).toEqual(false);
+      execTest(responseMock(200, '', mockContentType), false);
+      execTest(responseMock(200, mockResponseSmaller50, mockContentType), false);
     });
 
     it('Calling the module with max age different of null, httpRequest equal to 200, content size greather than 200 and content-type different of text/html, I expect that returns false', () => {
-      mock = responseMock(200, mockResponseGreather50, 'application/json');
-      expect(Cache.canSave('10s', mock)).toEqual(false);
-
-      mock = responseMock(200, mockResponseGreather50, 'text/css');
-      expect(Cache.canSave('10s', mock)).toEqual(false);
+      execTest(responseMock(200, mockResponseGreather50, 'application/json'), false);
+      execTest(responseMock(200, mockResponseGreather50, 'text/css'), false);
     });
 
     it('Calling the module with max age different of null, httpRequest equal to 200, content size greather than 200 and content-type equal to text/html, I expect that returns true', () => {
-      mock = responseMock(200, mockResponseGreather50, 'text/html');
-      expect(Cache.canSave('10s', mock)).toEqual(true);
+      execTest(responseMock(200, mockResponseGreather50, 'text/html'), true);
     });
   });
 });
