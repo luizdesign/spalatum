7373aac8326f21acbbf5cacd0ae7697ea2a7c5a6
Daniel Silva <daniel5055@gmail.com>
2017-10-25 16:57:17 -0200

    Filter unit tests

---
+++ __tests__/__unit__/template-assets-filter.test.js
@@ -0,0 +1,36 @@
+const module = require('../../lib/template-assets-filter.js');
+const templates = require('../../__mocks__/templates.js');
+
+describe.skip('Javascript assets filter', () => {
+  it('When execute the module with a template with no assets, I expect it doesn\'t change the content', () => {
+    const result = module(templates.simple);
+    expect(result).toBe(templates.simple);
+    expect(typeof result).toBe('string');
+    expect(module('')).toBe('');
+    expect(module()).toBe('');
+  });
+
+  it('When execute the module with a template with duplicated assets I expect it remove subsequent duplicates', () => {
+    const template = `
+    <section>
+      <header>
+        <script src="foo.com/react.js"></script>
+        <script src="foo.com/asset.js"></script>
+        <script src='foo.com/react.js'></script>
+        <script src="foo.com/asset.js"></script>
+      </header>
+    </section>
+    `;
+
+    const result = `
+    <section>
+      <header>
+        <script data-original src="foo.com/react.js"></script>
+        <script data-original src="foo.com/asset.js"></script>
+      </header>
+    </section>
+    `;
+
+    expect(module(template)).toBe(result);
+  });
+});
