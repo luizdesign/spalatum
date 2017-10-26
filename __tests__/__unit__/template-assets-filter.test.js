18133139d12c869d5fba9d09a1532d81b2a2dbd1
Allysson Santos <allysson.santos@catho.com>
2017-10-26 11:02:58 -0200

    Update regex and remove console.logs

--- __tests__/__unit__/template-assets-filter.test.js
+++ __tests__/__unit__/template-assets-filter.test.js
@@ -1,7 +1,7 @@
 const module = require('../../lib/template-assets-filter.js');
 const templates = require('../../__mocks__/templates.js');
 
-describe.skip('Javascript assets filter', () => {
+describe('Javascript assets filter', () => {
   it('When execute the module with a template with no assets, I expect it doesn\'t change the content', () => {
     const result = module(templates.simple);
     expect(result).toBe(templates.simple);
@@ -30,6 +30,13 @@ describe.skip('Javascript assets filter', () => {
       </header>
     </section>
     `;
+
+    expect(module(template)).toBe(result);
+  });
+
+  it('When execute the module with a minified template with duplicated assets, I expect it remove subsequent duplicates', () => {
+    const template = '<body><span>unit test</span><script src="foo.com/react.js"></script><script src="foo.com/asset.js"></script><script src="foo.com/react.js"></script><script src="foo.com/asset.js"></script></body>';
+    const result = '<body><span>unit test</span><script data-original src="foo.com/react.js"></script><script data-original src="foo.com/asset.js"></script></body>';
 
     expect(module(template)).toBe(result);
   });
