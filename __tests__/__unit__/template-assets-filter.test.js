3083aef4ef6eacecbab9a9dfbb43b52eba29687b
Daniel Silva <daniel.silva@catho.com>
2017-10-27 14:17:22 -0200

    Minify filtered template

--- __tests__/__unit__/template-assets-filter.test.js
+++ __tests__/__unit__/template-assets-filter.test.js
@@ -1,13 +1,25 @@
 const module = require('../../lib/template-assets-filter.js');
 const templates = require('../../__mocks__/templates.js');
 
+beforeEach(() => {
+  document.outerHTML = null;
+});
+
 describe('Javascript assets filter', () => {
   it('When execute the module with a template with no assets, I expect it doesn\'t change the content', () => {
-    const result = module(templates.simple);
-    expect(result).toBe('<!DOCTYPE html> <html lang=\"en\"> <head> <meta charset=\"UTF-8\"/> <title>Unit Test</title> </head> <body> <fragment href=\"http://localhost:8000/\"/> </body> </html>'); // eslint-disable-line no-useless-escape
-    expect(typeof result).toBe('string');
-    expect(module('')).toBe('');
-    expect(module()).toBe('');
+    document.outerHTML = module(templates.simple);
+    expect(document.outerHTML).toMatchSnapshot();
+    expect(typeof document.outerHTML).toBe('string');
+  });
+
+  it('When execute the module with an empty template, I expect it return an default empty html string', () => {
+    document.outerHTML = module('');
+    expect(document.outerHTML).toMatchSnapshot();
+  });
+
+  it('When execute the module with no template, I expect it return an default empty html string', () => {
+    document.outerHTML = module();
+    expect(document.outerHTML).toMatchSnapshot();
   });
 
   it('When execute the module with a template with duplicated javascript assets, I expect it remove subsequent duplicates', () => {
@@ -20,32 +32,32 @@ describe('Javascript assets filter', () => {
       </header>
     </section>`;
 
-    const result = '<section> <header> <script src=\"foo.com/react.js\"/> <script src=\"foo.com/asset.js\"/>   </header> </section>'; // eslint-disable-line no-useless-escape
-
-    expect(module(template)).toBe(result);
+    document.outerHTML = module(template);
+    expect(document.outerHTML).toMatchSnapshot();
   });
 
   it('When execute the module with a template with duplicated javascript assets with attributes and dirty content, I expect it remove subsequent duplicates without removing others content and  attributes', () => {
     const template = `<section>
-      <script src="foo.com/asset.js"/>
+      <script src="foo.com/asset.js"></script>
       <header>
         <script async src="foo.com/react.js"></script>
         <script src="foo.com/asset.js" defer></script>
         <h1>Unit test</h1>
-        <script src="foo.com/asset.js" defer/>
+        <script src="foo.com/asset.js" defer></script>
       </header>
       <script src="foo.com/asset.js"></script>
-      <script type="javascript" src='foo.com/react.js'/>
+      <script type="javascript" src='foo.com/react.js'></script>
     </section>`;
 
-    expect(module(template)).toBe('<section> <script src=\"foo.com/asset.js\"/> <header> <script async src=\"foo.com/react.js\"/>  <h1>Unit test</h1>  </header>   </section>'); // eslint-disable-line no-useless-escape
+    document.outerHTML = module(template);
+    expect(document.outerHTML).toMatchSnapshot();
   });
 
   it('When execute the module with a minified template with duplicated javascript assets, I expect it remove subsequent duplicates', () => {
     const template = '<body><span>unit test</span><script src="foo.com/react.js"></script><script src="foo.com/asset.js"></script><script src="foo.com/react.js"></script><script src="foo.com/asset.js"></script></body>';
-    const result = '<body><span>unit test</span><script src="foo.com/react.js"/><script src="foo.com/asset.js"/></body>';
 
-    expect(module(template)).toBe(result);
+    document.outerHTML = module(template);
+    expect(document.outerHTML).toMatchSnapshot();
   });
 });
 
@@ -53,38 +65,36 @@ describe('Styles assets filter', () => {
   it('When execute the module with a template with duplicated css assets, I expect it remove subsequent duplicate', () => {
     const template = `<section>
       <header>
-        <link href="foo.com/react.css" rel="stylesheet" media="print"/>
-        <link href="foo.com/asset.css" rel="stylesheet"/>
-        <link href="foo.com/react.css" rel="stylesheet" media="screen"/>
-        <link href="foo.com/asset.css" rel="stylesheet"/>
+        <link href="foo.com/react.css" rel="stylesheet" media="print">
+        <link href="foo.com/asset.css">
+        <link href="foo.com/react.css" rel="stylesheet" media="screen">
+        <link href="foo.com/asset.css" rel="stylesheet">
       </header>
     </section>`;
 
-    const result = '<section> <header> <link href=\"foo.com/react.css\" rel="stylesheet" media="print"/> <link href=\"foo.com/asset.css\" rel="stylesheet"/>   </header> </section>'; // eslint-disable-line no-useless-escape
-
-    expect(module(template)).toBe(result);
+    document.outerHTML = module(template);
+    expect(document.outerHTML).toMatchSnapshot();
   });
 
   it('When execute the module with a template with duplicated css assets with attributes and dirty content, I expect it remove subsequent duplicates without removing others content and attributes', () => {
     const template = `<section>
-      <link type="text/css" href="foo.com/react.css" rel="stylesheet" media="print"/>
+      <link type="text/css" href="foo.com/react.css" rel="stylesheet" media="print">
       <header>
-        <link href="foo.com/asset.css"/>
+        <link href="foo.com/asset.css">
         <p>Unit test</p>
-        <link href="foo.com/react.css" rel="stylesheet" media="screen"/>
+        <link href="foo.com/react.css" rel="stylesheet" media="screen">
       </header>
-      <link href="foo.com/asset.css" type="text/css"/>
+      <link href="foo.com/asset.css" type="text/css">
     </section>`;
 
-    const result = '<section> <link type=\"text/css\" href=\"foo.com/react.css\" rel=\"stylesheet\" media=\"print\"/> <header> <link href=\"foo.com/asset.css\"/> <p>Unit test</p>  </header>  </section>'; // eslint-disable-line no-useless-escape
-
-    expect(module(template)).toBe(result);
+    document.outerHTML = module(template);
+    expect(document.outerHTML).toMatchSnapshot();
   });
 
   it('When execute the module with a minified template with duplicated css assets, I expect it remove subsequent duplicates', () => {
-    const template = '<body><span>unit test</span><link href="foo.com/react.css"/><link href="foo.com/asset.css"><link href="foo.com/react.css"/><link href="foo.com/asset.css"/></body>';
-    const result = '<body><span>unit test</span><link href="foo.com/react.css"/><link href="foo.com/asset.css"/></body>';
+    const template = '<body><span>unit test</span><link href="foo.com/react.css"><link href="foo.com/asset.css"><link href="foo.com/react.css"><link href="foo.com/asset.css"></body>';
 
-    expect(module(template)).toBe(result);
+    document.outerHTML = module(template);
+    expect(document.outerHTML).toMatchSnapshot();
   });
 });
