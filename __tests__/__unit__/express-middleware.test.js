88e2eb8de2887bb069cc96c55ad58c8b3e612d3c
José Luiz Coé <josesouza@catho.com.br>
2017-11-06 11:31:17 -0200

    Starting headers request test using express middleware

--- __tests__/__unit__/express-middleware.test.js
+++ __tests__/__unit__/express-middleware.test.js
@@ -1,4 +1,18 @@
+const spalatum = require('../../lib');
+// const superagent = require('../../lib/fragment-request');
 const middleware = require('../../lib/express-middleware');
+// const responseMock = require('../../__mocks__/response');
+const templates = require('../../__mocks__/templates.js');
+
+// const mockSet = jest.fn().mockReturnThis();
+
+// const mockGet = (status, result, type) => {
+//   const mock = responseMock(status, result, type);
+//   mock.set = mockSet;
+//   mock.use = superagent.use;
+
+//   superagent.get = jest.fn().mockReturnValue(mock);
+// };
 
 describe('# When calling express middleware', () => {
   let res;
@@ -7,13 +21,24 @@ describe('# When calling express middleware', () => {
 
   beforeEach(() => {
     res = {};
-    req = {};
+    req = {
+      headers: {
+        cookie: 'foo',
+      },
+    };
     next = jest.fn();
   });
 
-  it.only(`Should call the "next" callback function,
+  it(`Should call the "next" callback function,
     passed as the third argument`, () => {
     middleware(req, res, next);
     expect(next).toHaveBeenCalledTimes(1);
+  });
+
+  it.skip('Should receive headers on the request object', async () => {
+    // mockGet(200, templates.simple, 'text/html');
+    const result = middleware(req, res, next);
+    await spalatum.render(templates.simple);
+    expect(result).toHaveBeenCalledWith(req.headers);
   });
 });
