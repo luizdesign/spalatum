f25144f8ca5c153722d437c6a879cc36119c2d04
José Luiz Coé <josesouza@catho.com.br>
2017-11-06 18:23:00 -0200

    Refactoring middleware tests

--- __tests__/__unit__/express-middleware.test.js
+++ __tests__/__unit__/express-middleware.test.js
@@ -1,44 +1,33 @@
 const spalatum = require('../../lib');
-// const superagent = require('../../lib/fragment-request');
+const superagent = require('../../lib/fragment-request');
 const middleware = require('../../lib/express-middleware');
-// const responseMock = require('../../__mocks__/response');
+
+// Mock utils
 const templates = require('../../__mocks__/templates.js');
-
-// const mockSet = jest.fn().mockReturnThis();
-
-// const mockGet = (status, result, type) => {
-//   const mock = responseMock(status, result, type);
-//   mock.set = mockSet;
-//   mock.use = superagent.use;
-
-//   superagent.get = jest.fn().mockReturnValue(mock);
-// };
+const mockServer = require('../../__mocks__/server.js')('./fragment.js');
+const mockRequest = require('../../__mocks__/request');
+const mockResponse = require('../../__mocks__/response');
 
 describe('# When calling express middleware', () => {
-  let res;
-  let req;
-  let next;
-
+  const next = jest.fn();
   beforeEach(() => {
-    res = {};
-    req = {
-      headers: {
-        cookie: 'foo',
-      },
-    };
-    next = jest.fn();
+    superagent.set = jest.spyOn(superagent, 'set');
+    mockServer.listen(8000);
+  });
+
+  afterEach(() => {
+    mockServer.close();
   });
 
   it(`Should call the "next" callback function,
     passed as the third argument`, () => {
-    middleware(req, res, next);
+    middleware(mockRequest, mockResponse, next);
     expect(next).toHaveBeenCalledTimes(1);
   });
 
-  it.skip('Should receive headers on the request object', async () => {
-    // mockGet(200, templates.simple, 'text/html');
-    const result = middleware(req, res, next);
+  it('Should set headers on the request object', async () => {
+    middleware(mockRequest, mockResponse, next);
     await spalatum.render(templates.simple);
-    expect(result).toHaveBeenCalledWith(req.headers);
+    expect(superagent.set).toHaveBeenCalledWith(mockRequest.headers);
   });
 });
